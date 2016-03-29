80d820588bb9e134d49dddbfa6ad72b2fcdfbcf4
Augustinas Malinauskas <augustinasmal@gmail.com>
2016-03-29 11:02:31 +0100

    Reduced to single iteration

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -22,22 +22,16 @@
    * Example: jarvis play
    * Will call module which has associated keyword "play", which in this case is VLC module.
    */
-  for(int i = 0; i < ActiveModules.size(); i++)
-  {
-      if(API::KeywordExists(CallData, ActiveModules[i]->getActivationKeywords()))
-      {
-          ActiveModules[i]->Execute(CallData);
-      }
-  }
-
-  /*
-    Handling speech syntesis for multiple modules
-  */
   std::string CompleteOutput = "";
   for(int i = 0; i < ActiveModules.size(); i++)
   {
       if(API::KeywordExists(CallData, ActiveModules[i]->getActivationKeywords()))
       {
+          ActiveModules[i]->Execute(CallData);
+
+	  /*
+	    Handling speech syntesis for multiple modules
+	  */
           if (ActiveModules[i]->OutputRequest())
             SynthesizeSpeech += ActiveModules[i]->getOutput();
       }
